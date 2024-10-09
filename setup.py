d2466608bee9eeb79d224585b5150b22c8c04c60
Artasov <ivanhvalevskey@gmail.com>
2024-10-09 18:52:31 +0300

    fix v0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="django-logui",
-    version="0.0.3",
+    version="0.0.4",
     author="xlartas",
     author_email="ivanhvalevskey@gmail.com",
     description="Flexible, fast and productive logging for Django with UI",
